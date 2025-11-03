54d29bcd2d08988c2fed91aeb7b5329053607a8f
dyleo16 <dylanleo160312@gmail.com>
2025-11-03 14:51:47 -0600

    Avances Dylan pt1

--- IF2000_Proyecto_Final/src/domain/Flight.java
+++ IF2000_Proyecto_Final/src/domain/Flight.java
@@ -1,4 +1,3 @@
-
 package domain;
 
 public class Flight {
@@ -6,31 +5,86 @@ public class Flight {
     private String destination;
     private Plane plane;
 
+    private String code;
+    private String departure;
+    
     // Constructor without plane
     public Flight(String origin, String destination) {
+        validateLocations(origin, destination);
         this.origin = origin;
         this.destination = destination;
     }
 
     // Constructor with plane
     public Flight(String origin, String destination, Plane plane) {
+        validateLocations(origin, destination);
         this.origin = origin;
         this.destination = destination;
         this.plane = plane;
     }
+    //Full constructor
+    public Flight(String code, String origin, String destination, String departure, Plane plane){
+    validateLocations(origin, destination);
+    this.code = code;
+    this.origin = origin;
+    this.destination = destination;
+    this.departure = departure;
+    this.plane = plane;
+    }
 
+    private void validateLocations(String origin, String destination){
+        if (origin == null || origin.isBlank())
+            throw new IllegalArgumentException("Empty origin");
+        if (destination != null && !destination.isBlank()) {
+        } else {
+            throw new IllegalArgumentException("Empty desnation");
+        }
+        if (origin.equalsIgnoreCase(destination))
+            throw new IllegalArgumentException("Origin and destination can't be the same");
+    }
+    
+    public void ensurePlane(){
+        if (plane == null) throw new IllegalStateException("The is no aircraft assigned to the flight");
+    }
+    
     // Check availability for a given class
     public boolean checkAvailability(String seatClass) {
+        ensurePlane();
         return plane.hasAvailability(seatClass);
     }
 
     // Reserve a seat for a given class
-    public void reserveSeat(String seatClass) {
-        plane.reserveSeat(seatClass);
+    public boolean reserveSeat(String seatClass) {
+        ensurePlane();
+        return plane.reserveSeat(seatClass);
     }
-
+    
+    public boolean occupySeat(String clase) { 
+        return reserveSeat(clase);
+    }
+    
     // Getters
     public String getOrigin() {
     return origin;
+    }
+    
+    public String getDestination(){
+    return destination;
+    }
+    
+    public String getCode(){
+    return code;
+    }
+    
+    public String getDeparture(){
+    return departure;
+    }
+    
+    public Plane getPlane(){
+    return plane;
+    }
+    
+    public void setPlane(Plane plane){
+    this.plane = plane;    
     }
 }
